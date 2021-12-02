b32b720ead21dc3d0f58a8bf5263598b191cc550
Random Geek <randomgeek78@gmail.com>
2021-12-02 11:50:37 -0800

    updates

--- init.sh
+++ init.sh
@@ -2,5 +2,10 @@ git config user.name "Random Geek"
 git config user.email randomgeek78@gmail.com
 
 if ! command -v "yadm" >& /dev/null; then
+  if command -v "apt" >& /dev/null; then
     sudo apt install -y yadm
+  elif command -v "dnf" >& /dev/null; then
+    sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:TheLocehiliosan:yadm/Fedora_33/home:TheLocehiliosan:yadm.repo
+    sudo dnf install -y yadm
+  fi
 fi
